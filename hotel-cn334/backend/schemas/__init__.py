72b369083844ec80791b054e1534da9504820201
Neroadawat <adawat.aua@gmail.com>
2026-03-28 17:38:15 +0700

    Add backend

---
+++ hotel-cn334/backend/schemas/__init__.py
@@ -0,0 +1,3 @@
+from schemas.booking import BookingCreate, BookingResponse, ContactInfo, DateRange, GuestCount
+from schemas.chat import ChatMessageCreate, ChatMessageResponse
+from schemas.room import RoomResponse
